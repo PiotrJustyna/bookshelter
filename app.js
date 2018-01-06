cbb7d25e431a968e3869b51af70a02793987d6e2
Piotr Justyna <piotr.justyna@gmail.com>
2018-01-06 23:47:07 +0000

    PCF confimuration, readme.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(bodyParser.urlencoded({
 }));
 
 var routes = require("./routes/routes.js")(app);
-
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
---
+++ readme.md
@@ -0,0 +1,6 @@
+# readme
+## api endpoints
+https://bookshelter.cfapps.io
+https://bookshelter.cfapps.io/book?author=stephen%20king&title=duma%20key
+## deployment
+```cf push bookshelter```
